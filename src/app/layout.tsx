cf38faebd539a1996db4eefa984cf9e646e06f94
imibtc <113363320+imibtc@users.noreply.github.com>
2025-12-23 23:04:19 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,9 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
 	return (
 		<html lang='en' suppressHydrationWarning style={htmlStyle}>
 			<Head />
-
+			
 			<body>
+				<script defer src="https://umami-dun-sigma.vercel.app/script.js" data-website-id="1cc38107-c89e-4b33-9ec2-e1ef333293dc"></script>
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
