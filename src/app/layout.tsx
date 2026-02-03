c13da26af17143d6b246ef31b66100be95cbdda8
imibtc <113363320+imibtc@users.noreply.github.com>
2026-02-03 23:10:46 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,10 +56,10 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
 
 				{/* AnythingLLM 智能问答机器人 */}
 				<script
-					data-embed-id="1a4d1597-7538-4dc1-b6d7-bbce60033bc5"
-					data-base-api-url="http://192.168.1.118:3001/api/embed"
-					src="http://192.168.1.118:3001/embed/anythingllm-chat-widget.min.js"
-				></script>
+					data-embed-id="62378cb2-cd34-43cc-be41-aeea649afa70"
+data-base-api-url="http://192.168.1.118:3001/api/embed"
+src="http://192.168.1.118:3001/embed/anythingllm-chat-widget.min.js">
+				</script>
 				
 			</body>
 		</html>
