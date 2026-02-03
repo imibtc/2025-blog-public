61f0655717a6d97bb7f494ef65743de4c9d821f3
imibtc <113363320+imibtc@users.noreply.github.com>
2026-02-03 23:15:28 +0800

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,10 +57,9 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
 				{/* AnythingLLM 智能问答机器人 */}
 				<script
 					data-embed-id="62378cb2-cd34-43cc-be41-aeea649afa70"
-data-base-api-url="http://192.168.1.118:3001/api/embed"
-src="http://192.168.1.118:3001/embed/anythingllm-chat-widget.min.js">
-				</script>
-				
+					data-base-api-url="http://192.168.1.118:3001/api/embed"
+					src="http://192.168.1.118:3001/embed/anythingllm-chat-widget.min.js"
+				></script>
 			</body>
 		</html>
 	)
